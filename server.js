b27679721ddfb3bb76ab1b09f3805947ea80a592
OladayoAgbolade <dayoagbolade@gmail.com>
2021-01-28 16:45:54 +0100

    Setup API testing

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const port = 3000;
+const app = express();
+app.use(express.json());
+
+app.use("/", require("./routes/api/home"));
+app.use("/validate-rule", require("./routes/api/validation"));
+
+app.listen(port, () => {
+  console.log(`App listening on http://localhost:${port}`);
+});
